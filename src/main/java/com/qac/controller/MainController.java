3d13b0f0af1bda056b6ca8e78144b3b022e6cc57
Joe Baldwin <joe.baldwin@capgemini.com>
2018-05-21 11:47:46 +0100

    Added file path parameter

--- src/main/java/com/qac/controller/MainController.java
+++ src/main/java/com/qac/controller/MainController.java
@@ -9,19 +9,23 @@ import com.qac.model.Queue;
 
 public class MainController {
     
+    private final static String configFilePath = System.getProperty("configFilePath");
+    
     public static void main(String[] args) {       
         
         final Logger logger = LogManager.getLogger();
         
         logger.info("Initialising Config");
         
-        List<Queue> queuesList = ConfigReader.readConfig("src/main/resources/queues.json");
+        List<Queue> queuesList = ConfigReader.readConfig(configFilePath);
         
         logger.info("Config Initialised");
         
         logger.info("Creating Queues");
         
         QueueGenerator.createQueues(queuesList);
+        
+        logger.info("Queues Created");
                 
     }
 
